ea5c797b4952afa6d93432ae9a489762daa570e6
Tomas Lopes <tomas050302@gmail.com>
2021-02-24 11:32:17 +0000

    feat: added survey entity

---
+++ src/controllers/SurveyController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+
+import SurveyRepository from '../repositories/SurveyRepository';
+
+export default {
+  async index(request: Request, response: Response) {
+    const surveyRepository = getCustomRepository(SurveyRepository);
+
+    const surveys = await surveyRepository.find();
+
+    return response.json(surveys);
+  },
+
+  async create(request: Request, response: Response) {
+    const { title, description } = request.body;
+
+    const surveyRepository = getCustomRepository(SurveyRepository);
+
+    const survey = surveyRepository.create({
+      title,
+      description
+    });
+
+    try {
+      const data = await surveyRepository.save(survey);
+
+      return response.status(201).json(data);
+    } catch (err) {
+      return response.status(400).json(err);
+    }
+  }
+};
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './controllers/UserController';
+import SurveyController from './controllers/SurveyController';
 
 const routes = Router();
 
 routes.post('/user', UserController.create);
+
+routes.get('/survey', SurveyController.index);
+routes.post('/survey', SurveyController.create);
 
 export default routes;
